01200041405c1fa5a2419ebf43176cfef23c2722
kabirladkat8421-lgmt <kabirladkat8421@gmail.com>
2026-08-23 17:21:12 +0530

    MultiDimensionArr.java Done

---
+++ src/work/MultiDimensionArr.java
@@ -0,0 +1,28 @@
+package work;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiDimensionArr {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[3][2];
+
+		for (int row = 0; row < arr.length; row++) {
+			for (int col = 0; col < arr[row].length; col++)
+				arr[row][col] = sc.nextInt();
+		}
+
+		/*for (int row = 0; row < arr.length; row++) {
+			for (int col = 0; col < arr[row].length; col++)
+				System.out.print(arr[row][col] + " ");
+			System.out.println();*/
+		for(int row = 0;row<arr.length;row++)
+		{
+			System.out.println(Arrays.toString(arr[row]));
+		}
+
+	}
+
+}
