958231599e8a0a257e32479d0ae29e983be2fdfe
kabirladkat8421-lgmt <kabirladkat8421@gmail.com>
2026-08-30 19:24:21 +0530

    FindMinNum.java and RangeSearch.java done

---
+++ src/dsalearning/FindMinNum.java
@@ -0,0 +1,23 @@
+package dsalearning;
+
+public class FindMinNum {
+
+	public static void main(String[] args) {
+		int[] arr = { -8,15,22,63,54,5};
+		System.out.println(min(arr));
+	}
+
+	public static int min(int[] arr) {
+		if (arr.length == 0) {
+			return -1;
+		}
+		int ans = arr[0]; 
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < ans)
+			{
+				ans = arr[i]; 
+			}
+		}
+		return ans;
+	}
+}
---
+++ src/dsalearning/RangeSearch.java
@@ -0,0 +1,24 @@
+package dsalearning;
+
+public class RangeSearch {
+
+	public static void main(String[] args) {
+		int[]arr= {10,20,30,40,50,60};
+		int target = 40;
+		System.out.println(searchinrange(arr,target,1,4));
+	}
+	public static int searchinrange(int[]arr,int target,int start,int end)
+	{
+		for(int index =start;index<=end;index++)
+		{
+			int element = arr[index];
+			if(element==target)
+			{
+				return index;
+			}
+			
+		}
+		return -1;
+	}
+	
+}
