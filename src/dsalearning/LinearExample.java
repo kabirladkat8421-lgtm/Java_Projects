80dc6dee182d568a7cf390a774ffc31ccfa4d1f3
kabirladkat8421-lgmt <kabirladkat8421@gmail.com>
2026-08-30 16:15:47 +0530

    LinearExample.java done

--- src/dsalearning/LinearExample.java
+++ src/dsalearning/LinearExample.java
@@ -3,28 +3,24 @@ package dsalearning;
 public class LinearExample {
 
 	public static void main(String[] args) {
-		int[]arr={10,20,30,50,60,70,80,90,100};
-		int ans = linearserch(arr,60);
+		int[] arr = { 10, 20, 30, 50, 60, 70, 80, 90, 100 };
+		int target = 60; //It's Shadowing the target
+		int ans = linearsearch(arr, target);
 		System.out.println(ans);
-	
+
 	}
 
-static int linearserch(int[]arr,int target)
-{
-	if(arr.length==0)
-	{
-		return -1;
-	}
-	for(int index =0;index<arr.length;index++)
-	{
-		int element = arr[index];
-		if(element==target)
-		{
-			return index;
+	static int linearsearch(int[] arr, int target) {
+		if (arr.length == 0) {
+			return -1;
 		}
+		for (int index = 0; index < arr.length; index++) {
+			int element = arr[index];
+			if (element == target) {
+				return index;
+			}
+		}
+		return -1;
+
 	}
-	return -1;
-
 }
-}
-
