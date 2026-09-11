316cd08a61434042e16a5712cab89a1ba5062245
kabirladkat8421-lgmt <kabirladkat8421@gmail.com>
2026-09-11 14:14:55 +0530

    InfinitArrayFindElement

--- src/binarysearch/FindFirstAndLast.java
+++ src/binarysearch/FindFirstAndLast.java
@@ -1,49 +1,46 @@
 package binarysearch;
-public class FindFirstAndLast extends Solution {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        int[] arr = {1, 7, 7, 8, 8, 9};
-        int target = 7;
-        int[]ans=s.searchRange(arr, target);
-        System.out.println(ans[0]+" "+ans[1]);
 
+import java.util.Arrays;
+
+class FindFirstAndLast {
+    public static void main(String[] args) {
+        int[] arr = {1, 7, 7, 8, 8, 9};//Array declartion and inilization
+        Solution sol = new Solution(); //object creation needed because we cannot acesses other class method if it's not static
+int target=7;//Target declaration it's shadowing other where target being used
+int[]ans=sol.Search(arr,target);//why craeted the ans array we need 2 element for storing two valuse so we declare ans as Array
+System.out.println(Arrays.toString(ans));//Displaying the ans Arrays
     }
 }
-class  Solution {
-    public int[] searchRange(int[] nums, int target) {
-
-        int[] ans = {-1, -1};
-        // check for first occurrence if target first
-        ans[0] = search(nums, target, true);
-        if (ans[0] != -1) {
-            ans[1] = search(nums, target, false);
+class Solution {
+    public int[] Search(int[] arr, int target) {
+        int[] ans = {-1, -1};//Returning the value if ans not found
+        ans[0] = binarySearch(arr, target, true);
+        if (ans[0] != -1) //checking the first element is not -1 if
+            {
+            ans[1] = binarySearch(arr, target, false);
         }
         return ans;
     }
-
-    // this function just returns the index value of target
-    int search(int[] nums, int target, boolean findStartIndex) {
+    public int binarySearch(int[] arr, int target, boolean FindFisrtElement) {
         int ans = -1;
         int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
-            int mid = start + (end - start) / 2;
 
-            if (target < nums[mid]) {
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
                 end = mid - 1;
-            } else if (target > nums[mid]) {
+            } else if (target > arr[mid]) {
                 start = mid + 1;
             } else {
-                // potential ans found
-                ans = mid;
-                if (findStartIndex) {
+                ans = mid;//pontential ans found
+                if (FindFisrtElement) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
             }
+
         }
         return ans;
     }
---
+++ src/binarysearch/InfiniteArrayFindElement.java
@@ -0,0 +1,40 @@
+package binarysearch;
+
+public class InfiniteArrayFindElement {
+    public static void main(String[] args) {
+    int[]arr={1,22,34,41,55,62,74,82,94};//Array declaration and inilization
+    int target=55;
+        System.out.println(arr[getRange(arr,target)]);
+
+    }
+    static int getRange(int[]arr,int target)
+    {
+        int start=0;//start point of Array
+        int end=1;//end point of Array
+        while(target>arr[end])//it's true until target>arr[end] end value will unitl it's false
+        {
+            int temp=end+1;//stroing end value in temp
+            end=end+(end-start+1)*2; //end value is being double
+            start=temp;//privious end is new start point
+        }
+        return binarySearch(arr,start,end,target);//returning the result of binarySearch
+    }
+    static int binarySearch(int[]arr,int start,int end,int target)
+    {
+while(start<=end)
+{
+    int mid=start+(end-start)/2;
+    if(target<arr[mid])
+    {
+        end=mid-1;
+    }
+    else if(target>arr[mid])
+    {
+        start=mid+1;
+    }else
+    {
+        return mid;
+    }
+}return -1;
+    }
+}
